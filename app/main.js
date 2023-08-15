5fecca65bbb72dbf00252a26280b731a391c82ee
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-08-15 16:16:20 -0300

    Feat: Dados dos livros inseridos dentro da aplicação.

---
+++ app/main.js
@@ -0,0 +1,15 @@
+let livros = [];
+
+const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+
+getBuscarLivrosDaApi();
+
+
+async function getBuscarLivrosDaApi() {
+    const res = await fetch(endpointDaApi);
+
+    livros = await res.json();
+
+    console.table(livros);
+    exibirOsLivrosNaTela(livros);
+}
